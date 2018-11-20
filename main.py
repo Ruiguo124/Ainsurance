870706c2c57fbe6f74b2844eb0ec7b2f9503a01f
Wail Ayad <wailay@hotmail.fr>
2018-11-19 19:04:58 -0500

    added epochs and batch size flags

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python
 import tensorflow as tf
 import numpy as np
 import pandas
--- videocapture.py
+++ videocapture.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import cv2
 import tensorflow as tf
 import numpy as np
